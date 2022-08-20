8708fda230de65242288036d8853fd33304a2112
Jefferson472 <jefferson472@hotmail.com>
2022-08-20 15:56:15 -0300

    revert:receita e despesa individual

--- src/api/contabilidade/models/Despesa.py
+++ src/api/contabilidade/models/Despesa.py
@@ -1,7 +1,17 @@
-from .BaseModel import BaseModel
+from django.db import models
 
 
-class Despesa(BaseModel):
+class Despesa(models.Model):
+    id = models.AutoField(primary_key=True)
+    descricao = models.CharField(max_length=255)
+    valor = models.FloatField(default=0)
+    create_at = models.DateTimeField(auto_now_add=True)
+    # TODO: verificar se a data criação fixa e data_att variável 
+    update_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.descricao
+    
     class Meta:
         verbose_name = 'Despesa'
         verbose_name_plural = 'Despesas'
--- src/api/contabilidade/models/Receita.py
+++ src/api/contabilidade/models/Receita.py
@@ -1,7 +1,17 @@
-from .BaseModel import BaseModel
+from django.db import models
 
 
-class Receita(BaseModel):
+class Receita(models.Model):
+    id = models.AutoField(primary_key=True)
+    descricao = models.CharField(max_length=255)
+    valor = models.FloatField(default=0)
+    create_at = models.DateTimeField(auto_now_add=True)
+    # TODO: verificar se a data criação fixa e data_att variável 
+    update_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.descricao
+
     class Meta:
         verbose_name = 'Receita'
         verbose_name_plural = 'Receitas'
