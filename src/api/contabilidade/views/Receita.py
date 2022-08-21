5713252cb9c15df02f62c9de56d0e4fe519bc36f
Jefferson472 <jefferson472@hotmail.com>
2022-08-20 21:05:33 -0300

    feat: validação para receitas duplicidas no mês

--- src/api/contabilidade/views/Receita.py
+++ src/api/contabilidade/views/Receita.py
@@ -1,4 +1,8 @@
-from rest_framework import generics
+from django.utils import timezone
+
+from rest_framework import generics, status
+from rest_framework.exceptions import ValidationError
+from rest_framework.response import Response
 
 from contabilidade.models.Receita import Receita
 from contabilidade.serializer.Receita import ReceitaSerializer
@@ -7,6 +11,17 @@ from contabilidade.serializer.Receita import ReceitaSerializer
 class ReceitaList(generics.ListCreateAPIView):
     queryset = Receita.objects.all()
     serializer_class = ReceitaSerializer
+
+    def perform_create(self, serializer):
+        description = serializer.validated_data.get('descricao')
+        current_month = timezone.now().month
+        queryset = self.queryset.filter(
+            descricao=description, create_at__month=current_month
+        )
+        if queryset:
+            raise ValidationError('Erro: Receita já cadastrada para este mês!')
+        else:
+            serializer.save()
 
 
 class ReceitaDetail(generics.RetrieveUpdateDestroyAPIView):
