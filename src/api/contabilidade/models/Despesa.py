aaf18b094a098386a7586fa8333c8d5793b7f574
Jefferson472 <jefferson472@hotmail.com>
2022-08-23 22:22:10 -0300

    feat:add categorias

--- src/api/contabilidade/models/Despesa.py
+++ src/api/contabilidade/models/Despesa.py
@@ -2,16 +2,29 @@ from django.db import models
 
 
 class Despesa(models.Model):
+    CATEGORIES = (
+        ('alimentacao', 'Alimentação'),
+        ('saude', 'Saúde'),
+        ('moradia', 'Moradia'),
+        ('transporte', 'Transporte'),
+        ('educacao', 'Educação'),
+        ('lazer', 'Lazer'),
+        ('imprevistos', 'Imprevistos'),
+        ('outras', 'Outras'),
+    )
+
     id = models.AutoField(primary_key=True)
     descricao = models.CharField(max_length=255)
     valor = models.FloatField(default=0)
+    categoria = models.CharField(
+        max_length=11, choices=CATEGORIES, default='Outras') 
     create_at = models.DateTimeField(auto_now_add=True)
-    # TODO: verificar se a data criação fixa e data_att variável 
-    update_at = models.DateTimeField(auto_now_add=True)
+    # TODO: verificar se a data criação fixa e data_att variável
+    update_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.descricao
-    
+
     class Meta:
         verbose_name = 'Despesa'
         verbose_name_plural = 'Despesas'
--- src/api/contabilidade/models/Receita.py
+++ src/api/contabilidade/models/Receita.py
@@ -6,8 +6,8 @@ class Receita(models.Model):
     descricao = models.CharField(max_length=255)
     valor = models.FloatField(default=0)
     create_at = models.DateTimeField(auto_now_add=True)
-    # TODO: verificar se a data criação fixa e data_att variável 
-    update_at = models.DateTimeField(auto_now_add=True)
+    # TODO: verificar se a data criação fixa e data_att variável
+    update_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.descricao
